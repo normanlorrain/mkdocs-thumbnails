9ea5cd7671287adaffae8c1ac683e831d077e853
Norman Lorrain <normanlorrain@gmail.com>
2021-04-19 11:08:58 -0600

    Packaging update.

--- setup.py
+++ setup.py
@@ -1,38 +1,7 @@
-from setuptools import setup, find_packages
+# This is needed for installing the project in editable mode
+# e.g. pip install -e .
 
 
-setup(
-    name='mkdocs-thumbnails',
-    version='0.1.0',
-    description='A MkDocs plugin.  Generates thumbnails for links in the markdown',
-    long_description='',
-    keywords='mkdocs',
-    url='',
-    author='Norman Lorrain ',
-    author_email='normanlorrain@gmail.com',
-    license='MIT',
-    python_requires='>=3.6',
-    install_requires=[
-        'mkdocs>=1.0.4',
-        'Pillow>=8.2.0',
-        'PyMuPDF>=1.18.12'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
-    ],
-    packages=find_packages(),
-    entry_points={
-        'mkdocs.plugins': [
-            'thumbnails = thumbnails.plugin:ThumbnailMaker'
-        ]
-    }
-)
+import setuptools
+
+setuptools.setup()
