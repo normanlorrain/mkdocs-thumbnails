f662a7b3885cdf3037444918a87e85a62117fa95
Norman Lorrain <normanlorrain@gmail.com>
2023-06-05 13:18:36 -0600

    Black.

--- src/thumbnails/plugin.py
+++ src/thumbnails/plugin.py
@@ -12,28 +12,29 @@ from bs4 import BeautifulSoup
 from . import thumbnail
 
 
-# Goal: have a means in our markdown to indicate the desire for PDF links 
+# Goal: have a means in our markdown to indicate the desire for PDF links
 # to have a thumbnail, automatically generated.
 #
-# Method: use a special attribute (supported by Python's Attribute Lists) 
+# Method: use a special attribute (supported by Python's Attribute Lists)
 # https://python-markdown.github.io/extensions/attr_list/
-# 
-# eg 
+#
+# eg
 #    [foo](bar.pdf){.pdf}   (Markdown)
 # becomes
 #    <a href="bar.pdf" class="pdf">foo</a>   (HTML)
-# in the on_page_content() below, we can search the HTML for this pattern and 
+# in the on_page_content() below, we can search the HTML for this pattern and
 # modify the HTML before it's written to disk, AND, generate the required thumbnail
 # eg:
 #    <a href="bar.pdf" class="pdf">foo</a>
 # becomes
-#    <a href="bar.pdf"><img src="bar.pdf-thumb.png" />foo</a> 
+#    <a href="bar.pdf"><img src="bar.pdf-thumb.png" />foo</a>
 
-log = logging.getLogger('mkdocs')
+log = logging.getLogger("mkdocs")
+
 
 class ThumbnailMaker(BasePlugin):
-    # style is added to the image tag.  e.g. margin-right:10px; 
-    config_scheme = ( ('style', config_options.Type(str, default="")), )
+    # style is added to the image tag.  e.g. margin-right:10px;
+    config_scheme = (("style", config_options.Type(str, default="")),)
 
     def on_pre_build(self, **kwargs):
         self.pdf_count = 0
@@ -42,62 +43,66 @@ class ThumbnailMaker(BasePlugin):
     # This is done for each page.  The markdown conversion has been done at this point.
     # Modify the HTML to have the thumbnail when the relevant attribute is found
     def on_page_content(self, html, **kwargs):
-        sys.stdout.write('.')
-        sys.stdout.flush()                # flush stdout buffer (actual character display)
-    
-        pageFile = kwargs['page'].file
+        sys.stdout.write(".")
+        sys.stdout.flush()  # flush stdout buffer (actual character display)
+
+        pageFile = kwargs["page"].file
         srcDir = Path(pageFile.abs_src_path).parent
         tgtDir = Path(pageFile.abs_dest_path).parent
 
         soup = BeautifulSoup(html, "html.parser")
 
-
         # PDF links
         links = soup.find_all("a", class_="pdf")
-        self.pdf_count+=len(links)
+        self.pdf_count += len(links)
         for link in links:
-            href = link.get('href')
-            filename = urllib.parse.unquote(href)     # removes %20's typically
-            if not (srcDir/Path(filename)).exists():
-                continue # Skip this file; mkdocs detects these already, so no need to log again.
-            thumbnail.createPdfThumb(srcDir/Path(filename), tgtDir/Path(filename+"-thumb.png"))
-            img = soup.new_tag('img')
-            img['src'] = f"{href}-thumb.png"
-            img['style'] = self.config["style"]
-            img['class'] = "pdf"
-            del link['class']  
-            link.contents.insert(0,img)
+            href = link.get("href")
+            filename = urllib.parse.unquote(href)  # removes %20's typically
+            if not (srcDir / Path(filename)).exists():
+                continue  # Skip this file; mkdocs detects these already, so no need to log again.
+            thumbnail.createPdfThumb(
+                srcDir / Path(filename), tgtDir / Path(filename + "-thumb.png")
+            )
+            img = soup.new_tag("img")
+            img["src"] = f"{href}-thumb.png"
+            img["style"] = self.config["style"]
+            img["class"] = "pdf"
+            del link["class"]
+            link.contents.insert(0, img)
 
         # YouTube links
         # See https://webapps.stackexchange.com/questions/54443/format-for-id-of-youtube-video
         #
         links = soup.find_all("a", class_="youtube")
-        self.yt_count+=len(links)
+        self.yt_count += len(links)
         for link in links:
-            href = link.get('href')
+            href = link.get("href")
             try:
-                if 'playlist' in href:
-                    id = re.search( r'.*playlist\?list=([0-9A-Za-z_-]+).*', href).group(1)
-                    thumbnail.createYouTubePlaylistThumb(href,tgtDir/Path(id+"-thumb.png"))
+                if "playlist" in href:
+                    id = re.search(r".*playlist\?list=([0-9A-Za-z_-]+).*", href).group(
+                        1
+                    )
+                    thumbnail.createYouTubePlaylistThumb(
+                        href, tgtDir / Path(id + "-thumb.png")
+                    )
                 else:
-                    id = re.search( r'.*youtu.be\/([0-9A-Za-z_-]+).*', href).group(1)
-                    thumbnail.createYouTubeThumb(id, tgtDir/Path(id+"-thumb.png"))
+                    id = re.search(r".*youtu.be\/([0-9A-Za-z_-]+).*", href).group(1)
+                    thumbnail.createYouTubeThumb(id, tgtDir / Path(id + "-thumb.png"))
             except thumbnail.HTTPError as e:
-                    log.warn(f'Bad Youtube link: {href} in {pageFile.abs_src_path} ')
-                    continue
-            img = soup.new_tag('img')
-            img['src'] = f"{id}-thumb.png"
-            img['style'] = self.config["style"]
-            img['class'] = "youtube"
-            del link['class']
-            link.contents.insert(0,img)
+                log.warn(f"Bad Youtube link: {href} in {pageFile.abs_src_path} ")
+                continue
+            img = soup.new_tag("img")
+            img["src"] = f"{id}-thumb.png"
+            img["style"] = self.config["style"]
+            img["class"] = "youtube"
+            del link["class"]
+            link.contents.insert(0, img)
 
         return str(soup)
 
     def on_post_build(self, **kwargs):
-        sys.stdout.write('\b')            # erase the last written char
-        sys.stdout.flush()                # flush stdout buffer (actual character display)
+        sys.stdout.write("\b")  # erase the last written char
+        sys.stdout.flush()  # flush stdout buffer (actual character display)
 
         log.info(f"YouTube count: {self.yt_count}")
         log.info(f"PDF count: { self.pdf_count}")
-
--- src/thumbnails/thumbnail.py
+++ src/thumbnails/thumbnail.py
@@ -10,60 +10,62 @@ import fitz
 from PIL import Image
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from requests.exceptions import *
+
 _session = requests_cache.CachedSession()
 
 
-def createPdfThumb(inputPDFfile,outputThumbFile):        
+def createPdfThumb(inputPDFfile, outputThumbFile):
     # Extract image
     doc = fitz.open(inputPDFfile)  # open document
     pix = doc.get_page_pixmap(0, alpha=False)  # render page to an image
 
     # Resize image
     im = Image.frombytes("RGB", (pix.width, pix.height), pix.samples)
-    im = im.crop((72,72,im.width-72,im.height-72))  # left, upper, right, and lower 
+    im = im.crop(
+        (72, 72, im.width - 72, im.height - 72)
+    )  # left, upper, right, and lower
     im.thumbnail((128, 128))
 
     # Create directory paths if necessary
     if not outputThumbFile.parent.exists():
-        outputThumbFile.parent.mkdir(parents=True, exist_ok = True)
+        outputThumbFile.parent.mkdir(parents=True, exist_ok=True)
 
     # Save image to file
     im.save(str(outputThumbFile))
 
+
 def createYouTubeThumb(id, outputThumbFile):
     # Create directory paths if necessary
     if not outputThumbFile.parent.exists():
-        outputThumbFile.parent.mkdir(parents=True, exist_ok = True)
+        outputThumbFile.parent.mkdir(parents=True, exist_ok=True)
 
-    # Save image 
-    with open(outputThumbFile,'wb') as outfile:
-         # Get image from YouTube
-        url = f'https://img.youtube.com/vi/{id}/default.jpg'
+    # Save image
+    with open(outputThumbFile, "wb") as outfile:
+        # Get image from YouTube
+        url = f"https://img.youtube.com/vi/{id}/default.jpg"
         response = _session.get(url)
         response.raise_for_status()
         imageBytes = response.content
         outfile.write(imageBytes)
-        
-    
+
+
 # Playlists are a little harder; need to get the thumbnail via the oembed api
 def createYouTubePlaylistThumb(playlistUrl, outputThumbFile):
     # Create directory paths if necessary
     if not outputThumbFile.parent.exists():
-        outputThumbFile.parent.mkdir(parents=True, exist_ok = True)
+        outputThumbFile.parent.mkdir(parents=True, exist_ok=True)
 
-    # Save image 
-    with open(outputThumbFile,'wb') as outfile:
-        # Get URL of thumbnail from playlist url 
-        response = _session.get(f'https://youtube.com/oembed?url={playlistUrl}&format=json')
+    # Save image
+    with open(outputThumbFile, "wb") as outfile:
+        # Get URL of thumbnail from playlist url
+        response = _session.get(
+            f"https://youtube.com/oembed?url={playlistUrl}&format=json"
+        )
         response.raise_for_status()
         playlistData = response.json()
-        
-        # Get thumbnail 
-        response = _session.get(playlistData['thumbnail_url'])
+
+        # Get thumbnail
+        response = _session.get(playlistData["thumbnail_url"])
         response.raise_for_status()
 
         outfile.write(response.content)
-    
-
-
-
